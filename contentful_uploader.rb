dec2af0284e38770c05267948b08d76ba46bea24
Yuka Kato <satoimo.to.imo@gmail.com>
2018-06-28 14:37:19 +0900

    メソッドにした

--- contentful_uploader.rb
+++ contentful_uploader.rb
@@ -23,12 +23,28 @@ class ContentfulUploader
     @entries = entries
   end
 
+  def client
+    @client ||= Contentful::Management::Client.new(ENV['CTF_CMA_ACCESS_TOKEN'])
+  end
+
+  def blog_space
+    @blog_space ||= client.spaces.find(ENV['CTF_SPACE_ID'])
+  end
+
+  def environment
+    @environment ||= blog_space.environments.find('master')
+  end
+
+  def entries
+    @entries ||= environment.content_types.find(ENV['CTF_CONTENT_TYPE_ID']).entries
+  end
+
   # upload! をよんだときに、
   # create のパラメータに blog_post の各値を渡されてよばれたかを
   # テストする、というふうにかける
   # have_recieved というのをつかえる
   def upload!(blog_post)
-    @entries.create(
+    entries.create(
       title: blog_post.title,
       body: blog_post.body,
       sourceFileName: blog_post.source_file_name,
