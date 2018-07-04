383a384add048901319ac197453a9f621ca8d7ae
Yuka Kato <satoimo.to.imo@gmail.com>
2018-07-05 00:39:28 +0900

    動かすのに必要なファイルを読み込んだ

--- blog_post_builder.rb
+++ blog_post_builder.rb
@@ -1,5 +1,6 @@
 require "pry"
 require "time"
+require_relative "./blog_post"
 
 class BlogPostBuilder
   def build(blog_post_hash)
--- upload.rb
+++ upload.rb
@@ -1,3 +1,5 @@
+require_relative "./blog_post_builder.rb"
+require_relative "./contentful_uploader.rb"
 # ARGV とかで JSON のありかのディレクトリを文字列で指定できるようにするといいかも（デフォルトの位置は決めておいて、「指定することもできるよ」という状態にする
 # 変えられないところはなるべく少なくしておくべき
 blog_post_builder = BlogPostBuilder.new
