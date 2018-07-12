ec1ed3081c05753046db94a033ee1c1382c73da0
Yuka Kato <satoimo.to.imo@gmail.com>
2018-07-12 16:45:49 +0900

    不要なコメントを削除した

--- blog_post_builder.rb
+++ blog_post_builder.rb
@@ -4,13 +4,6 @@ require_relative "./blog_post"
 
 class BlogPostBuilder
   def build(blog_post_hash)
-    # いったんここで File.read して
-    # あとから別のメソッドにする
-    # ここでは new するだけにしたい
-    # まずはひとつの json を扱えるようにして
-    # そのあと（どこかで）Dir.glob するなりするようにする
-    # なのでいったん File.read の引数はハードコーディング
-
     BlogPost.new(
       title: blog_post_hash["title"],
       body: blog_post_hash["bodyContent"],
@@ -22,7 +15,6 @@ class BlogPostBuilder
   private
 
   def created_at(source_base)
-    # Timezone を明示的に指定したほうがいい（TZ が違う PC でこのテストを実行したら落ちる）
     Time.parse(source_base.sub(/(\A[\d]{4}-[\d]{2}-[\d]{2}).*/){ $1 }).to_s
   end
 end
--- upload.rb
+++ upload.rb
@@ -1,7 +1,6 @@
 require_relative "./blog_post_builder.rb"
 require_relative "./contentful_uploader.rb"
-# ARGV とかで JSON のありかのディレクトリを文字列で指定できるようにするといいかも（デフォルトの位置は決めておいて、「指定することもできるよ」という状態にする
-# 変えられないところはなるべく少なくしておくべき
+
 blog_post_builder = BlogPostBuilder.new
 uploader = ContentfulUploader.new(entries)
 
